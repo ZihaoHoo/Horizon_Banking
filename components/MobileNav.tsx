ee9140ad72cab65c0420667f3fe6c189bca876d3
HOO ZI HAO <2003929C@student.tp.edu.sg>
2024-05-13 08:54:43 +0800

    implement auth, apprwrite and sentry

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -13,6 +13,7 @@ import { cn } from "@/lib/utils"
 import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import Footer from "./Footer";
 
 
 const MobileNav = ({user}: MobileNavProps) => {
@@ -24,35 +25,32 @@ const MobileNav = ({user}: MobileNavProps) => {
           <Image src='/icons/hamburger.svg' width={30} height={30} alt="menu" className="cursor-pointer"/>
         </SheetTrigger>
         <SheetContent side='left' className="border-none bg-white gap-1 px-4">
-        <Link href='/'
-            className='mb-12 cursor-pointer flex item-center gap-2'>
-                <Image 
-                      src='/icons/logo.svg'
-                      width={34} height={34} alt={'Horizon log'}/>
-                <h1 className='text-26 font-ibm-plex-serif font-bold text-black-1'>Horizon</h1>
-            </Link>
-            <div className="mobilenav-sheet">
-              <SheetClose asChild>
-                <nav className="flex h-full flex-col gap-6 pt-16 text-white">
-                  {sidebarLinks.map((item)=>{
-                    const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
-                    return(
-                      <SheetClose asChild key={item.route}>
-                        <Link href={item.route} key={item.label}
-                        className={cn('mobilenav-sheet_close w-full',{'bg-bank-gradient':isActive})}>
+        <Link href='/' className='mb-12 cursor-pointer flex item-center gap-2'>
+          <Image src='/icons/logo.svg'width={34} height={34} alt={'Horizon log'}/>
+          <h1 className='text-26 font-ibm-plex-serif font-bold text-black-1'>Horizon</h1>
+        </Link>
+        <div className="mobilenav-sheet">
+          <SheetClose asChild>
+            <nav className="flex h-full flex-col gap-6 pt-16 text-white">
+              {sidebarLinks.map((item)=>{
+                const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
+                return(
+                  <SheetClose asChild key={item.route}>
+                    <Link href={item.route} key={item.label}
+                    className={cn('mobilenav-sheet_close w-full',{'bg-bank-gradient':isActive})}>
 
-                            <Image src={item.imgURL} alt={item.label} width={20} height={20} 
-                            className={cn({'brightness-[3] invert-0':isActive})}/>
+                        <Image src={item.imgURL} alt={item.label} width={20} height={20} 
+                        className={cn({'brightness-[3] invert-0':isActive})}/>
 
-                        <p className={cn('text-16 font-semibold text-black-2',{'!text-white' :isActive})}>{item.label}</p>
-                        </Link>
-                      </SheetClose>
-                      )}
+                    <p className={cn('text-16 font-semibold text-black-2',{'!text-white' :isActive})}>{item.label}</p>
+                    </Link>
+                    </SheetClose>
                     )}
-                    USER
+                  )}
+                  USER
                 </nav>
-              </SheetClose>
-              FOOTER
+                </SheetClose>
+              <Footer user={user} type='mobile'/>
             </div> 
         </SheetContent>
       </Sheet>
